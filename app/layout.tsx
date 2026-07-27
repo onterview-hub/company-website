9fc0cce65289e356e4706547b6e82155bafd35e6
sketchon <sketchon@daum.net>
2026-07-27 11:00:38 +0900

    구글 애널리틱스 연동

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -44,7 +45,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-9M7FVPFZ2N"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-9M7FVPFZ2N');
+          `}
+        </Script>
+        {children}
+      </body>
     </html>
   );
 }
